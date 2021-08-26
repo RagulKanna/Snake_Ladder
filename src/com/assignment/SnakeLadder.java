7faa258cf1239fe461136457f523071907d5f474
RagulKanna <ragulragi619@gmail.com>
2021-08-26 14:50:53 +0530

    Added: Welcome to Snake ladder game

---
+++ src/com/assignment/SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.assignment;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("        Welcome to the Snake & Ladder Game      ");
+
+        int Player_Position=0;
+        System.out.println("\nThe Position of the player at starting is "+Player_Position);
+
+    }
+}
