0fa06eee13c445dc62027ff60618459d267be6d7
RagulKanna <ragulragi619@gmail.com>
2021-08-26 16:10:37 +0530

    Added: Player_position reach up to 100 or above

--- src/com/assignment/SnakeLadder.java
+++ src/com/assignment/SnakeLadder.java
@@ -3,8 +3,11 @@ package com.assignment;
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("        Welcome to the Snake & Ladder Game      ");
+
         int Player_Position=0,dicenum;
 
+        for(int i=1; Player_Position <=99;i++)
+        {
             System.out.println("\nThe Position of the player at starting is " + Player_Position);
             dicenum = (int) (Math.floor(Math.random() * 6 % 7 + 1));
 
@@ -16,19 +19,20 @@ public class SnakeLadder {
 
             switch (play) {
                 case 0:
-                    System.out.println("\nThere is no play for player, so the positiion is "+Player_Position);
+                    System.out.println("\nThere is no play for player, so the positiion is " + Player_Position);
                     break;
                 case 1:
-                    Player_Position=Player_Position+dicenum;
-                    System.out.println("\nThe player position get changed to :"+Player_Position);
+                    Player_Position = Player_Position + dicenum;
+                    System.out.println("\nThe player position get changed to " + Player_Position);
                     break;
                 case 2:
-                    if(Player_Position>dicenum)
-                        Player_Position=Player_Position-dicenum;
+                    if (Player_Position > dicenum)
+                        Player_Position = Player_Position - dicenum;
                     else
-                        Player_Position=0;
-                    System.out.println("\nThe player position get changed to :"+Player_Position);
+                        Player_Position = 0;
+                    System.out.println("\nThe player position get changed to " + Player_Position);
                     break;
+            }
 
             }
         }
