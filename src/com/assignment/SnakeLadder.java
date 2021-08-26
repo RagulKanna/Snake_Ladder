be3b0d97cacadbd7ed789bdf9f371048a5eb70fc
RagulKanna <ragulragi619@gmail.com>
2021-08-26 15:10:32 +0530

    Added: value getting while rolling dice

--- src/com/assignment/SnakeLadder.java
+++ src/com/assignment/SnakeLadder.java
@@ -6,6 +6,7 @@ public class SnakeLadder {
 
         int Player_Position=0;
         System.out.println("\nThe Position of the player at starting is "+Player_Position);
-
+        int dicenum=(int)(Math.floor(Math.random() * 6 % 7 +1));
+        System.out.println("\nThe Number got while rolling dice "+dicenum);
     }
 }
