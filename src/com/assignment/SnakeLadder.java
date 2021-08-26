02905105b9d7d8ee3082c9f268a3d75750cf0ba5
RagulKanna <ragulragi619@gmail.com>
2021-08-26 16:04:00 +0530

    Added: option to play incase of Snake,Ladder,Noplay

--- src/com/assignment/SnakeLadder.java
+++ src/com/assignment/SnakeLadder.java
@@ -3,10 +3,37 @@ package com.assignment;
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("        Welcome to the Snake & Ladder Game      ");
+        int Player_Position=0,dicenum;
 
-        int Player_Position=0;
-        System.out.println("\nThe Position of the player at starting is "+Player_Position);
-        int dicenum=(int)(Math.floor(Math.random() * 6 % 7 +1));
-        System.out.println("\nThe Number got while rolling dice "+dicenum);
+            System.out.println("\nThe Position of the player at starting is " + Player_Position);
+            dicenum = (int) (Math.floor(Math.random() * 6 % 7 + 1));
+
+
+            int play = (int) (Math.random() * 3);
+            if (play == 1 || play == 2)
+                System.out.println("\nThe Number got while rolling dice " + dicenum);
+
+
+            switch (play) {
+                case 0:
+                    System.out.println("\nThere is no play for player, so the positiion is "+Player_Position);
+                    break;
+                case 1:
+                    Player_Position=Player_Position+dicenum;
+                    System.out.println("\nThe player position get changed to :"+Player_Position);
+                    break;
+                case 2:
+                    if(Player_Position>dicenum)
+                        Player_Position=Player_Position-dicenum;
+                    else
+                        Player_Position=0;
+                    System.out.println("\nThe player position get changed to :"+Player_Position);
+                    break;
+
+            }
+        }
     }
-}
+
+
+
+
